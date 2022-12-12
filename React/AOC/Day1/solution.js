1fc1a0e9d2f952d5c4ce792571baeb8e925bcf23
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-12-12 20:11:36 +0000

    Day1AOC

---
+++ React/AOC/Day1/solution.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+
+let current = 0;
+let totals = [];
+
+try {
+  let data = fs.readFileSync("input.txt").toString();
+  const answer = data
+    .trim()
+    .split("\n")
+    .map((el) => {
+      const item = parseInt(el);
+      if (isNaN(item)) {
+        totals.push(current);
+        current = 0;
+      } else current = current + item;
+    });
+  totals.sort((a, b) => a - b).reverse();
+  //Part1
+  console.log(totals[0]);
+  //Part 2
+  console.log(totals[0] + totals[1] + totals[2]);
+} catch (e) {
+  console.log("Error:", e.stack);
+}
